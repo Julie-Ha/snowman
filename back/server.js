ce454f6189d795aa3907845ef6b2f156c185d404
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-10-14 14:21:50 +0200

    first commit

---
+++ back/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+let a = ''
+let myJson =''
+
+
+const { Connection, query } = require('stardog');
+ 
+const conn = new Connection({
+  username: 'admin',
+  password: 'admin',
+  endpoint: 'http://localhost:5820',
+});
+ 
+query.execute(conn, 'myDB', 'select * where {?c rdf:type :Cell}', 'application/sparql-results+json', {
+  reasoning:true
+}).then(({ body }) => {
+   a = body.results.bindings;
+  myJson = JSON.stringify(a);
+});
+
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(myJson)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
---
+++ front/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="cell of cells" :key="cell.c.value">
+        {{ cell.c.value }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  data: function() {
+    return {
+      cells: [],
+    };
+  },
+  methods: {
+    loadCells() {
+      axios
+        .get("http://localhost:3000/")
+        .then((response) => {
+          this.cells = response.data;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  },
+  created() {
+    this.loadCells();
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
